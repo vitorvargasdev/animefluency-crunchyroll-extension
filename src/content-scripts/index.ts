004a2840a55bb285a143973eae347f3837f45744
vitorvargasdev <50197635+vitorvargasdev@users.noreply.github.com>
2025-03-26 09:40:23 -0300

    refactor: change functions and var names to improve readability

--- src/content-scripts/index.ts
+++ src/content-scripts/index.ts
@@ -7,26 +7,27 @@ const checkElements = () => {
   return !!app && app.childElementCount > 0;
 };
 
+const mutationHandler = async (mutations: MutationRecord[]) => {
+  const changedVideo = "display: flex; height: 100%; width: 100%;";
+  const isStyle = mutations[0].attributeName === "style";
+  const isChangedVideo =
+    (mutations[0].target as HTMLElement).getAttribute("style") === changedVideo;
+
+  if (isStyle && isChangedVideo && !checkElements()) {
+    console.info("Injecting...");
+    await inject();
+  }
+
+  if (isStyle && isChangedVideo && checkElements()) {
+    console.info("Injection already completed; skipping...");
+  }
+};
+
 const startObservation = () => {
-  observer = new MutationObserver(async (mutations) => {
-    const changedVideo = "display: flex; height: 100%; width: 100%;";
-    const isStyle = mutations[0].attributeName === "style";
-    const isChangedVideo =
-      (mutations[0].target as HTMLElement).getAttribute("style") ===
-      changedVideo;
+  observer = new MutationObserver(mutationHandler);
 
-    if (isStyle && isChangedVideo && !checkElements()) {
-      console.info("Injecting...");
-      await inject();
-    }
-
-    if (isStyle && isChangedVideo && checkElements()) {
-      console.info('Injection already completed; skipping...');
-    }
-  });
-
-  const player = document.getElementById("player0") as HTMLElement;
-  observer.observe(player, {
+  const playerElement = document.getElementById("player0") as HTMLElement;
+  observer.observe(playerElement, {
     attributes: true,
   });
 };
