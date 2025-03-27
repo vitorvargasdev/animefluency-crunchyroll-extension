3ad6040eeb2f7c46c1b2ed8a0851944c554ab373
vitorvargasdev <50197635+vitorvargasdev@users.noreply.github.com>
2025-03-27 12:54:26 -0300

    feat: implement player store

--- src/player/App.vue
+++ src/player/App.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
+import { usePlayerStore } from "./stores";
+import { onMounted } from "vue";
+
+const playerStore = usePlayerStore();
+
+onMounted(() => {
+  playerStore.load();
+});
 </script>
 
 <template>
   <div>
-    <div class="text-red-900 text-bold text-2xl p-4">Hello 世界! I'm running inside crunchyroll player!</div>
+    <div class="text-red-900 text-bold text-2xl p-4">
+      Hello 世界! I'm running inside crunchyroll player! <br />
+      {{ playerStore.state }} <br />
+      {{ playerStore.currentProgress }}
+    </div>
   </div>
 </template>
 
